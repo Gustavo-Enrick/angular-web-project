e49e6c73e619a9e2881c5edc948c09226fe4771e
Gustavo Enrick <gustavoenrick16@gmail.com>
2023-10-19 00:35:03 -0300

    Add files

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":1,
+    "title":"gears-5,o melhor ?",
+    "description":"Gears 5 é um jogo de tiro em terceira pessoa desenvolvido pela The Coalition e publicado pela Xbox Game Studios. Lançado em 2019, o jogo é o quinto título da popular série Gears of War. Ele apresenta uma campanha emocionante que segue a história de Kait Diaz em sua busca por respostas sobre sua ascendência.",
+    "photoCover":"https://sm.ign.com/ign_pt/cover/g/gears-5/gears-5_b1hc.jpg"
+  },
+
+  {
+    "id":2,
+    "title":"A volta de Master Chief !",
+    "description":"Halo Infinite é um aguardado jogo de tiro em primeira pessoa da franquia Halo desenvolvido pela 343 Industries e publicado pela Xbox Game Studios. Lançado em dezembro de 2021, o jogo é um marco na série e promete trazer de volta o icônico Master Chief em uma aventura espacial emocionante. Com gráficos impressionantes e um mundo aberto expansivo chamado Anel, os jogadores podem esperar uma experiência de jogo mais ampla e interconectada.",
+    "photoCover":"https://sm.ign.com/ign_br/game/h/halo-infin/halo-infinite_dryz.jpg"
+  },
+  {
+    "id":3,
+    "title":"Sunset Overdrive,o jogo injustiçado !",
+    "description":"Sunset Overdrive é um jogo de ação em terceira pessoa desenvolvido pela Insomniac Games e lançado em 2014. Ambientado em um mundo pós-apocalíptico colorido e caótico, o jogo se destaca por sua jogabilidade única e estilo de humor irreverente. Os jogadores são desafiados a navegar pela cidade enquanto enfrentam hordas de mutantes usando uma variedade de armas malucas e manobras acrobáticas.",
+    "photoCover":"https://s2-techtudo.glbimg.com/VAIsxwmPEWosO8l3FR3TnUrKUd0=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/E/U/R2VySeQSy3Grc1DmBaMQ/2014-07-16-sunset-overdrive-30740-1920x1080.jpg"
+  },
+  {
+    "id":4,
+    "title":"A Plague Tale-Innocence,um jogo de se apegar.",
+    "description":"A Plague Tale: Innocence é um jogo de aventura desenvolvido pela Asobo Studio e lançado em 2019. Ambientado na França do século XIV, o jogo segue a jornada de dois irmãos, Amicia e Hugo, em meio a uma praga devastadora e uma Inquisição brutal. A narrativa emocional e cativante é um dos pontos fortes do jogo, explorando os laços familiares em um mundo sombrio.",
+    "photoCover":"https://meups.com.br/wp-content/uploads/2019/05/A-PLAGUE-TALE-INNOCENCE.jpg"
+  }
+]
